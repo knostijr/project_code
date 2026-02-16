87dee932f370f829896eb36a50d906b78d621370
knostijr <christophkonst@icloud.com>
2026-02-16 21:11:12 +0100

    Add admin in orders app

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,17 @@
-from django.contrib import admin
+"""
+Admin configuration for orders app.
+"""
 
-# Register your models here.
+from django.contrib import admin
+from .models import Order
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    """
+    Admin configuration for Order model.
+    """
+    list_display = ['id', 'title', 'customer_user', 'business_user', 'status', 'created_at']
+    list_filter = ['status', 'created_at']
+    search_fields = ['title', 'customer_user__username', 'business_user__username']
+    readonly_fields = ['created_at', 'updated_at']
